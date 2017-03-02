a0b870331ae727b542f7fd1adbb135e13206f98f
Elendow <elendow@gmail.com>
2017-03-03 00:14:53 +0100

    Added explanation to the event example

--- Source/Assets/SpritedowAnimator/Examples/EventExample.cs
+++ Source/Assets/SpritedowAnimator/Examples/EventExample.cs
@@ -12,9 +12,14 @@ public class EventExample : MonoBehaviour
     private void Awake()
     {
         particles = GetComponentInChildren<ParticleSystem>();
+
+        // Here we add an event to the 9th frame of the animation Slimedow
+        // After that, we subscribe the method "BurstParticles"
+        // We can also get the event with spriteAnimator.GetCustomEvent("Slimedow", 9) and suscribe whatever we want
         spriteAnimator = GetComponent<SpriteAnimator>();
         spriteAnimator.AddCustomEvent("Slimedow", 9).AddListener(BurstParticles);
 
+        // Then we simply play the animation and the magic begins
         spriteAnimator.Play(false);
     }
 

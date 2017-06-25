4953f3c00eeca7d533aaa69b2a8e26c91535a474
Elendow <elendow@gmail.com>
2017-06-25 17:32:51 +0200

    Little platformer example

--- Source/Assets/SpritedowAnimator/Examples/PlatformerExample/Scripts/PlayerPlatformer.cs
+++ Source/Assets/SpritedowAnimator/Examples/PlatformerExample/Scripts/PlayerPlatformer.cs
@@ -1,16 +1,55 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Elendow.SpritedowAnimator.Examples
 {
     public class PlayerPlatformer : MonoBehaviour
     {
         private SpriteAnimator spriteAnimator;
+        private Rigidbody2D rigidBody;
 
         private void Awake()
         {
             spriteAnimator = GetComponent<SpriteAnimator>();
+            rigidBody = GetComponent<Rigidbody2D>();
+        }
+
+        private void Update()
+        {
+            if(Input.GetKey(KeyCode.LeftArrow))
+            {
+                rigidBody.AddForce(Vector2.left * 50);
+                spriteAnimator.FlipSpriteX(true);
+            }
+            else if (Input.GetKey(KeyCode.RightArrow))
+            {
+                rigidBody.AddForce(Vector2.right * 50);
+                spriteAnimator.FlipSpriteX(false);
+            }
+            else if(Input.GetKey(KeyCode.DownArrow) && rigidBody.velocity.y == 0)
+            {
+                spriteAnimator.Play("AlienDuck");
+            }
+            else
+            {
+                spriteAnimator.Play("AlienStand");
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                rigidBody.AddForce(Vector2.up * 500);
+            }
+
+            if (!spriteAnimator.CurrentAnimation.Equals("AlienDuck"))
+            {
+                if (rigidBody.velocity.y != 0)
+                {
+                    spriteAnimator.Play("AlienJump");
+                }
+                else if (rigidBody.velocity.x != 0)
+                {
+                    spriteAnimator.Play("AlienWalk");
+                }
+            }
         }
     }
 }
